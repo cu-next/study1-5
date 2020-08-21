ad84b9d65596637ebb14b3c868b8aaa9dad00ea0
Ham <gkatmdvk@gmail.com>
2020-08-21 20:45:10 +0900

    Revert "Revert "초기상태""
    This reverts commit 0dc059f2307eea81f7067c0679294a6c95764eff.

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("캐릭터 설정")]
+    public float speed = 3;
+    public int jumpMaxNum = 2;
+    public float playerJumpPower = 15;
+    public float playerMaxHP = 100;
+    public float invincibilityTime = 1;
+    public JoystickControl script_JoystickControl;
+    public Image playerHPImage;
+
+    float playerHP;
+    float PlayerHP
+    {
+        set
+        {
+            playerHP = value;
+            
+            if (playerHP > playerMaxHP)
+            {
+                playerHP = playerMaxHP;
+            }
+        }
+        get
+        {
+            return playerHP;
+        }
+    }
+
+    public bool isNoJump = false;
+
+    int animState = 0;
+    int jumpCount = 0;
+    bool isInvincibility = false;
+    SpriteRenderer characterRender;
+    Animator characterAnimator;
+    Rigidbody2D characterRig;
+
+    void Start()
+    {
+        PlayerHP = playerMaxHP;
+        characterRender = GetComponent<SpriteRenderer>();
+        characterAnimator = GetComponent<Animator>();
+        characterRig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        Move();
+        bool isGround = GroundCheck();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UI_JumpButton();
+        }
+        else if (Mathf.Abs(characterRig.velocity.y) < 1&& isGround)
+        {
+            jumpCount = 0;
+        }
+        characterAnimator.SetInteger("JumpCount", jumpCount);
+        characterAnimator.SetInteger("AnimState", animState);
+        characterAnimator.SetBool("IsGround", isGround);
+
+        playerHPImage.fillAmount = (PlayerHP / playerMaxHP);
+        if (PlayerHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Move()
+    {
+        float moveVaule = script_JoystickControl.GetJoystickVecValue.x;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            moveVaule = Input.GetAxisRaw("Horizontal");
+
+        if (moveVaule != 0)
+        {
+            characterRender.flipX = moveVaule < 0;
+            animState = 1;
+        }
+        else
+        {
+            animState = 0;
+        }
+        //transform.localPosition += new Vector3(moveVaule * Time.deltaTime * speed, 0, 0);
+
+        if (Mathf.Abs(characterRig.velocity.x) < speed)
+        {
+            characterRig.AddForce(new Vector2(50 * moveVaule, 0));
+        }
+        else
+        {
+            characterRig.velocity = new Vector2(speed * moveVaule, characterRig.velocity.y);
+        }
+    }
+    public void UI_JumpButton()
+    {
+        if (jumpCount < jumpMaxNum)
+        {
+            if (jumpCount == 0)
+                characterAnimator.SetTrigger("Jump");
+            Jump(playerJumpPower, false);
+            jumpCount++;
+        }
+    }
+    public void Jump(float jumpPower,bool isStopJump)
+    {
+        if (isStopJump)
+        {
+            jumpCount = 2; 
+            characterAnimator.SetTrigger("Trampoline");
+        }
+        //characterRig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        characterRig.velocity = Vector2.up * jumpPower;        
+    }
+    bool GroundCheck()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red, 0);
+        int layerMask = 1 << LayerMask.NameToLayer("Ground");
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.1f, layerMask);
+    }
+    IEnumerator Invincibility()
+    {
+        isInvincibility = true;
+        //yield return new WaitForSeconds(invincibilityTime);
+
+        float timer = invincibilityTime;
+        float blinkingSpeed = 10;
+        SpriteRenderer playerRenderer = GetComponent<SpriteRenderer>();
+        while (true)
+        {
+            playerRenderer.color = new Color(1, 1, 1, Mathf.Abs(Mathf.Sin(timer * blinkingSpeed)));
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                break;
+            }
+            yield return null;
+        }
+        playerRenderer.color = new Color(1, 1, 1, 1);
+
+        isInvincibility = false;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Item"))
+        {
+            if (col.GetComponent<SpriteRenderer>().sprite.name == "apple")
+            {
+                Destroy(col.transform.gameObject);
+                PlayerHP += 10;
+            }
+            else if (col.GetComponent<SpriteRenderer>().sprite.name == "hp")
+            {
+                Destroy(col.transform.gameObject);
+                PlayerHP += 50;
+            }
+        }
+        else if (col.CompareTag("Enemy") && !isInvincibility)
+        {
+            PlayerDamage(20);
+        }
+    }
+    public void PlayerDamage(float value)
+    {
+        StartCoroutine(Invincibility());
+        characterAnimator.SetTrigger("Hit");
+        PlayerHP -= value;
+    }
+}
---
+++ Assets/Script/TrapsControl.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapsControl : MonoBehaviour
+{
+    public enum TrapsType
+    {
+        Non, FallingPlatforms, Fire, Trampoline, SpikeHead
+    }
+    public TrapsType trapsType;
+    bool isTrapStop = false;
+    public Vector2 rewr;
+    public float attackSpeed = 100;
+    public float backSpeed = 20;
+    bool isAttack = false;
+    Rigidbody2D enemyRir;
+    Vector2 disValue = Vector2.zero;
+    private void Awake()
+    {
+        enemyRir = GetComponent<Rigidbody2D>();
+        if (trapsType == TrapsType.SpikeHead)
+        {
+            StartCoroutine(EnemyAI());
+        }
+    }
+    IEnumerator EnemyAI()
+    {
+        Transform playerTr = GameObject.Find("Player").transform;
+        float checkDisValue = 10;
+        Vector2 startPos = transform.position;
+        while (playerTr)
+        {
+            if (!isAttack)
+            {
+                if (Vector2.Distance(transform.position, startPos) != 0)
+                {
+                    enemyRir.velocity = Vector2.zero;
+                    transform.position = Vector2.MoveTowards(transform.position, startPos, Time.deltaTime * backSpeed);
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, Vector2.left * checkDisValue, Color.red, 0);
+                    Debug.DrawRay(transform.position, Vector2.right * checkDisValue, Color.red, 0);
+                    Debug.DrawRay(transform.position, Vector2.up * checkDisValue, Color.red, 0);
+                    Debug.DrawRay(transform.position, Vector2.down * checkDisValue, Color.red, 0);
+                    disValue = Vector2.zero;
+                    int layerMask = 1 << LayerMask.NameToLayer("Player");
+                    if (Physics2D.Raycast(transform.position, Vector2.left, checkDisValue, layerMask))
+                    {
+                        disValue = Vector2.left;
+                    }
+                    else if (Physics2D.Raycast(transform.position, Vector2.right, checkDisValue, layerMask))
+                    {
+                        disValue = Vector2.right;
+                    }
+                    else if (Physics2D.Raycast(transform.position, Vector2.up, checkDisValue, layerMask))
+                    {
+                        disValue = Vector2.up;
+                    }
+                    else if (Physics2D.Raycast(transform.position, Vector2.down, checkDisValue, layerMask))
+                    {
+                        disValue = Vector2.down;
+                    }
+
+                    if (disValue != Vector2.zero)
+                    {
+                        Attack();
+                        isAttack = true;
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log(gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Ready"));
+                if (!gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Ready"))
+                {
+                    gameObject.GetComponent<Animator>().SetInteger("AttackDirection", -1);
+                    gameObject.GetComponent<Animator>().SetTrigger("Attack");
+                    yield return new WaitForSeconds(1);
+                }
+                else
+                {
+                    enemyRir.velocity = disValue * attackSpeed;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        if (isTrapStop)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            ContactPoint2D contact = col.contacts[0];
+            Vector3 pos = (Vector2)transform.position - contact.point;
+
+            Debug.Log(col.transform.tag);
+            switch (trapsType)
+            {
+                case TrapsType.FallingPlatforms:
+                    if (col.transform.position.y > transform.position.y + 0.4f)
+                    {
+                        gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                    }
+                    isTrapStop = true;
+                    break;
+                case TrapsType.Fire:
+                    if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Fire_Idle") && pos.normalized.y < -0.7f)
+                    {
+                        gameObject.GetComponent<Animator>().SetTrigger("Working");
+                    }
+                    break;
+                case TrapsType.Trampoline:
+                    if (col.transform.position.y > transform.position.y + 0.4f)
+                    {
+                        col.transform.GetComponent<PlayerControl>().Jump(25, true);
+                        gameObject.GetComponent<Animator>().SetTrigger("Working");
+                    }
+                    break;
+                case TrapsType.SpikeHead:
+                    col.transform.GetComponent<PlayerControl>().PlayerDamage(0); 
+                    Attack();
+                    isAttack = false;
+                    break;
+            }
+        }
+        else if (col.transform.CompareTag("Ground"))
+        {
+            switch (trapsType)
+            {
+                case TrapsType.SpikeHead:
+                    Attack();
+                    isAttack = false;
+                    break;
+            }
+        }
+
+    }
+    void Attack()
+    {
+        int attackNum = 0;
+        if (disValue == Vector2.left)
+        {
+            attackNum = 0;
+        }
+        else
+        {
+            attackNum = 1;
+        }
+        if (disValue == Vector2.up)
+        {
+            attackNum = 2;
+        }
+        else
+        {
+            attackNum = 3;
+        }
+        gameObject.GetComponent<Animator>().SetInteger("AttackDirection",attackNum);
+    }
+}
